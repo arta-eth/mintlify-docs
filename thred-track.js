b74c955e7a927f22488e2a74085ef91862e8a2a2
Arta <128208448+arta-eth@users.noreply.github.com>
2026-03-08 12:24:50 -0400

    Removed async attribute from script element in thred-track.js to ensure synchronous loading of the Thred tracking script.

--- thred-track.js
+++ thred-track.js
@@ -2,7 +2,6 @@
   function loadThredScript() {
     var script = document.createElement('script');
     script.src = 'https://cdn.thred.dev/thred-track.js?browserKey=0ec3ca8a-1507-4848-958e-477b175fcc69&debug=true';
-    script.async = true;
     
     script.onload = function() {
       console.log('Thred tracking script loaded successfully');
