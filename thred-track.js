36fbc8b3813af64cf95940ddfd25a300b8137f57
Arta <128208448+arta-eth@users.noreply.github.com>
2026-03-08 10:47:35 -0400

    Refactored thred-track.js to load the script asynchronously and append it to the body. Added a check for document readiness to ensure proper loading.

--- thred-track.js
+++ thred-track.js
@@ -1,6 +1,14 @@
 (function() {
-  var script = document.createElement('script');
-  script.src = 'https://cdn.thred.dev/thred-track.js?browserKey=0ec3ca8a-1507-4848-958e-477b175fcc69&debug=true';
-  script.defer = true;
-  document.head.appendChild(script);
+  function loadThredScript() {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.thred.dev/thred-track.js?browserKey=0ec3ca8a-1507-4848-958e-477b175fcc69';
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', loadThredScript);
+  } else {
+    loadThredScript();
+  }
 })();
