09cb16bc2e1fcaf15a1f5533002cdbfad613c5ed
Arta <128208448+arta-eth@users.noreply.github.com>
2026-03-08 12:21:22 -0400

    Updated thred-track.js to include debug parameter in script source URL for enhanced troubleshooting.

--- thred-track.js
+++ thred-track.js
@@ -1,7 +1,7 @@
 (function() {
   function loadThredScript() {
     var script = document.createElement('script');
-    script.src = 'https://cdn.thred.dev/thred-track.js?browserKey=0ec3ca8a-1507-4848-958e-477b175fcc69';
+    script.src = 'https://cdn.thred.dev/thred-track.js?browserKey=0ec3ca8a-1507-4848-958e-477b175fcc69&debug=true';
     script.async = true;
     
     script.onload = function() {
