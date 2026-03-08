e80203198a590002b4a876e5eb927e319fa56494
Arta <128208448+arta-eth@users.noreply.github.com>
2026-03-08 10:44:02 -0400

    Updated documentation with edits made through Mintlify web editor

---
+++ thred-track.js
@@ -0,0 +1,6 @@
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.thred.dev/thred-track.js?browserKey=0ec3ca8a-1507-4848-958e-477b175fcc69';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
